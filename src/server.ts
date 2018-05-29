c2e37e0685a7097e796e59ac1fb97c1ac9fac5e4
Dave Keen <dev@ruffness.com>
2018-05-29 11:15:05 +0200

    Added logging and a regexp flag to test URLs against before rendering

--- src/server.ts
+++ src/server.ts
@@ -17,14 +17,21 @@ const argv = yargs
   .option("port", {
     default: 80,
     describe: "The port the pdf server will listen on."
+  })
+  .option("regexp", {
+    describe: "A regular expression that the URL must match in order to be accepted."
   });
 
-const { host, port } = yargs.argv;
+const { host, port, regexp } = yargs.argv;
 
 app.get("/", async (req, res) => {
   try {
     if (!req.query.url) {
       throw new Error(`Missing url parameter`);
+    }
+
+    if (regexp && !new RegExp(regexp).test(req.query.url)) {
+      throw new Error(`${req.query.url} does not match the required format "${regexp}"`);
     }
 
     const browser = await puppeteer.launch();
@@ -34,6 +41,9 @@ app.get("/", async (req, res) => {
     await browser.close();
 
     res.contentType("pdf").send(pdf);
+
+    // Write a log saying we rendered this
+    console.log(`${(new Date()).toUTCString()}: ${req.query.url}`);
   } catch (err) {
     console.error(err.toString());
     res.json({ success: false, error: err.toString() }).status(500);
